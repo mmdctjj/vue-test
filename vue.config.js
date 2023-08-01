afc5bffabe16be1b67d62da9f1986ba29b20d1a9
mmdctjj <jiangpengjie@bizseer.com>
2023-08-01 20:45:07 +0800

    新增插件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('remove-others-console-loader')
+      .test(/\.vue|js$/)
+      .use('remove-others-console-loader')
+      .loader('remove-others-console-loader')
+  }
 })
